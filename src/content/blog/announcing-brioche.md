6167a6d959f967032d25cfa2d0eb0f663a5e0548
Kyle Lacy <kylelacy@kyle.space>
2024-05-18 19:40:24 -0700

    Set up simple blog (with placeholder post)

---
+++ src/content/blog/announcing-brioche.md
@@ -0,0 +1,6 @@
+---
+title: Announcing Brioche
+pubDate: 2024-05-18T19:41:35-07:00
+---
+
+Hello world!
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,12 @@
-import { defineCollection } from "astro:content";
+import { z, defineCollection } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
+  blog: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+    }),
+  }),
 };
