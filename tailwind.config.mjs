861228a0f9db60064a9c8f1d09693c4cb17a7eec
Kyle Lacy <kylelacy@kyle.space>
2024-06-01 00:51:42 -0700

    Set up Tailwind Typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 import colors from "tailwindcss/colors";
 import starlightPlugin from "@astrojs/starlight-tailwind";
+import tailwindTypography from "@tailwindcss/typography";
 
 // Color palette originally generated from this page:
 // https://starlight.astro.build/guides/css-and-tailwind/
@@ -29,11 +30,61 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}"],
   theme: {
     extend: {
+      typography: ({ theme }) => ({
+        brioche: {
+          css: {
+            "--tw-prose-body": theme("colors.gray[800]"),
+            "--tw-prose-headings": theme("colors.gray[900]"),
+            "--tw-prose-lead": theme("colors.gray[700]"),
+            "--tw-prose-links": theme("colors.gray[900]"),
+            "--tw-prose-bold": theme("colors.gray[900]"),
+            "--tw-prose-counters": theme("colors.gray[600]"),
+            "--tw-prose-bullets": theme("colors.gray[400]"),
+            "--tw-prose-hr": theme("colors.gray[300]"),
+            "--tw-prose-quotes": theme("colors.gray[900]"),
+            "--tw-prose-quote-borders": theme("colors.gray[300]"),
+            "--tw-prose-captions": theme("colors.gray[700]"),
+            "--tw-prose-code": theme("colors.gray[900]"),
+            "--tw-prose-pre-code": theme("colors.gray[100]"),
+            "--tw-prose-pre-bg": theme("colors.gray[900]"),
+            "--tw-prose-th-borders": theme("colors.gray[300]"),
+            "--tw-prose-td-borders": theme("colors.gray[200]"),
+            "--tw-prose-invert-body": theme("colors.gray[200]"),
+            "--tw-prose-invert-headings": theme("colors.white"),
+            "--tw-prose-invert-lead": theme("colors.gray[300]"),
+            "--tw-prose-invert-links": theme("colors.white"),
+            "--tw-prose-invert-bold": theme("colors.white"),
+            "--tw-prose-invert-counters": theme("colors.gray[400]"),
+            "--tw-prose-invert-bullets": theme("colors.gray[600]"),
+            "--tw-prose-invert-hr": theme("colors.gray[700]"),
+            "--tw-prose-invert-quotes": theme("colors.gray[100]"),
+            "--tw-prose-invert-quote-borders": theme("colors.gray[700]"),
+            "--tw-prose-invert-captions": theme("colors.gray[400]"),
+            "--tw-prose-invert-code": theme("colors.white"),
+            "--tw-prose-invert-pre-code": theme("colors.gray[300]"),
+            "--tw-prose-invert-th-borders": theme("colors.gray[600]"),
+            "--tw-prose-invert-td-borders": theme("colors.gray[700]"),
+          },
+        },
+        quoteless: {
+          css: {
+            "blockquote p:first-of-type::before": { content: "none" },
+            "blockquote p:first-of-type::after": { content: "none" },
+            blockquote: {
+              fontWeight: "inherit",
+              fontStyle: "inherit",
+            },
+            "blockquote p": {
+              opacity: "75%",
+            },
+          },
+        },
+      }),
       colors: {
         accent,
         gray,
       },
     },
   },
-  plugins: [starlightPlugin()],
+  plugins: [starlightPlugin(), tailwindTypography()],
 };
