935b855135e736aeb49537ea3b84407082ecee30
Kyle Lacy <kylelacy@kyle.space>
2025-01-19 03:21:03 -0800

    Update `install.sh` to support installing packed builds

--- public/install.sh
+++ public/install.sh
@@ -1,5 +1,40 @@
 #!/usr/bin/env sh
 
+auto_detect_install_type() {
+    case "$(uname -s)" in
+        Linux)
+            # Detect default path for glibc interpreter path
+            case "$(uname -m)" in
+                x86_64)
+                    default_interp_path="/lib64/ld-linux-x86-64.so.2"
+                    ;;
+                aarch64)
+                    default_interp_path="/lib/ld-linux-aarch64.so.1"
+                    ;;
+                *)
+                    echo "Could not determine GNU interpreter path for architecture $(uname -m)" >&2
+                    exit 1
+                    ;;
+            esac
+
+            interp_version="$("$default_interp_path" --version 2>&1 || true)"
+
+            if [ "$(uname -o)" != "GNU/Linux" ]; then
+                echo "Detected non-GNU platform, installing packed build" >&2
+                echo "packed"
+            elif ! echo "$interp_version" | grep -qi "GLIBC"; then
+                echo "Detected non-GNU platform, installing packed build" >&2
+                echo "packed"
+            else
+                echo "bin"
+            fi
+
+            ;;
+        *)
+            echo "bin"
+    esac
+}
+
 # Wrap the installation in a function so it only runs once the
 # entire script is downloaded
 install_brioche() {
@@ -16,27 +51,44 @@ install_brioche() {
     fi
 
     # The directory where Brioche gets installed
+    install_dir="${BRIOCHE_INSTALL_DIR:-$HOME/.local/bin}"
 
-    # Get the URL based on the kernel and architecture
-    case "$(uname -s)" in
-        Linux)
-            case "$(uname -m)" in
-                x86_64)
-                    brioche_url="https://releases.brioche.dev/v0.1.4/x86_64-linux/brioche"
-                    checksum="90a1366eabf63e04c559b77436b315736da8b73ce5b8e3b30eb5ac69b7e3c494"
-                    ;;
-                *)
-                    echo "Sorry, Brioche isn't currently supported on your architecture"
-                    echo "  Detected architecture: $(uname -m)"
-                    exit 1
-                    ;;
-            esac
+    # The directory where to unpack the installation, when using a packed build
+    unpack_dir="${BRIOCHE_INSTALL_UNPACK_DIR:-$HOME/.local/libexec/brioche}"
+
+    # Whether to install a packed build or a standalone binary
+    install_type="${BRIOCHE_INSTALL_TYPE:-auto}"
+
+    # Auto-detect installation type
+    if [ "$install_type" = "auto" ]; then
+        install_type="$(auto_detect_install_type)"
+    fi
+
+    # Validate installation type
+    case "$install_type" in
+        bin | packed)
             ;;
         *)
-            echo "Sorry, Brioche isn't currently supported on your operating system"
-    install_dir="${BRIOCHE_INSTALL_DIR:-$HOME/.local/bin}"
-            echo "  Detected kernel: $(uname -s)"
+            echo "Unsupported installation type: $install_type" >&2
             exit 1
+            ;;
+    esac
+
+    # Get the URL based on the kernel, architecture, and installation type
+    case "$(uname -sm) $install_type" in
+        "Linux x86_64 bin")
+            brioche_url="https://releases.brioche.dev/v0.1.4/x86_64-linux/brioche"
+            checksum="90a1366eabf63e04c559b77436b315736da8b73ce5b8e3b30eb5ac69b7e3c494"
+            ;;
+        "Linux x86_64 packed")
+            brioche_url="https://releases.brioche.dev/v0.1.4/brioche-packed/x86_64-linux/brioche-packed-x86_64-linux.tar.gz"
+            checksum="bc9aea0416b74f163bd801e87beb8b11eb99a5336700829eb8319ba9b612e06f"
+            ;;
+        *)
+            echo "Sorry, Brioche isn't currently supported on your platform"
+            echo "  Detected kernel: $(uname -s)"
+            echo "  Detected architecture: $(uname -m)"
+            echo "  Installation type: $install_type"
             ;;
     esac
 
@@ -49,21 +101,50 @@ install_brioche() {
     echo "  Checksum: $checksum"
     echo
 
-    # Download to a temporary path first
-    echo "Downloading to \`$brioche_temp/brioche\`..."
-    curl --proto '=https' --tlsv1.2 -fL "$brioche_url" -o "$brioche_temp/brioche"
+    # Download the file to a temporary path
+    temp_download="$brioche_temp/brioche-dl.tmp"
+    echo "Downloading to \`$temp_download\`..."
+    curl --proto '=https' --tlsv1.2 -fL "$brioche_url" -o "$temp_download"
     echo
 
     # Validate the checksum
     echo "Validating checksum..."
-    echo "$checksum  $brioche_temp/brioche" | sha256sum -c -
+    echo "$checksum  $temp_download" | sha256sum -c -
     echo
 
-    # Install to the target directory
-    echo "Installing to \`$install_dir/brioche\`..."
-    mkdir -p "$install_dir"
-    chmod +x "$brioche_temp/brioche"
-    mv "$brioche_temp/brioche" "$install_dir/brioche"
+    case "$install_type" in
+        bin)
+            # Install to the target directory
+            echo "Installing to \`$install_dir/brioche\`..."
+            mkdir -p "$install_dir"
+            chmod +x "$temp_download"
+            mv "$temp_download" "$install_dir/brioche"
+            ;;
+        packed)
+            echo "******************************************************************************" >&2
+            echo "** Heads up! Packed builds are experimental and don't support all features! **" >&2
+            echo "******************************************************************************" >&2
+
+            echo
+
+            # Unpack tarfile into the unpack directory
+            echo "Unpacking to \`$unpack_dir\`..."
+            rm -rf "$unpack_dir"
+            mkdir -p "$unpack_dir"
+            tar -xzf "$brioche_temp/brioche-dl.tmp" --strip-components=1 -C "$unpack_dir"
+
+            # Add a symlink in the install directory to the binary
+            # within the unpacked dir
+            symlink_target="$unpack_dir/bin/brioche"
+            echo "Adding symlink \`$install_dir/brioche\` -> \`$symlink_target\`..."
+            mkdir -p "$install_dir"
+            ln -sf "$symlink_target" "$install_dir/brioche"
+            ;;
+        *)
+            echo "Unexpected installation type: $install_type" >&2
+            exit 1
+            ;;
+    esac
 
     echo "Installation complete!"
 
