fb597fbced16a39554e00fdb3f75450b3847a613
Kyle Lacy <kylelacy@kyle.space>
2025-10-29 22:52:08 -0700

    Optionally show blurb about Brioche at the start of blog posts

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -5,6 +5,7 @@ export const BlogEntry = z.object({
   author: z.string(),
   authorUrl: z.string(),
   pubDate: z.date().or(z.null()),
+  showBlurb: z.boolean().optional(),
 });
 export type BlogEntry = z.infer<typeof BlogEntry>;
 
